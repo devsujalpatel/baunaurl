0798f3d770f981e224cac90c5723b2f657792972
devsujalpatel <sujal7455@gmail.com>
2025-09-03 02:28:28 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv"
+
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT! ?? 8080;
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
