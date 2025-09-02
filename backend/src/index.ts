2db458855a81b191447f0e7f737b448616c1e1ca
devsujalpatel <sujal7455@gmail.com>
2025-09-03 03:54:28 +0530

    drizzle configured

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,12 @@ dotenv.config();
 
 const PORT = process.env.PORT! ?? 8080;
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
